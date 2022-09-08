feaabb3ba9406efd5b5ace1ba9b2ebe61a4204e0
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2022-09-08 08:16:35 -0700

    final

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ function Home({ setPage }) {
     <motion.div
       animate={repo}
     >
-      <div className="md:hidden absolute w-screen overflow-clip -top-10 -z-10"><Svg/></div>
+      <div className={`md:hidden absolute -top-10 -z-10 w-[${window.innerWidth}] overflow-x-hidden`}><Svg/></div>
       <div className="hidden md:block md:absolute -z-10 -top-10 w-screen"><Svg2 /></div>
 
       <motion.div
