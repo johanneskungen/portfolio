18be6cf6e9f01d4453f74fbab65450a310773f1c
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-06-01 15:34:48 +0200

    redone

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import PROJECTS from "../projects.json";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 function Projects() {
   const router = useRouter();
@@ -17,20 +18,27 @@ function Projects() {
         <div className="grid grid-cols-3 gap-4 mt-4">
           {p.map((app) => (
             <div
-              className="cursor-pointer hover:scale-105 duration-300 max-w-sm h-[16rem] bg-white flex flex-col justify-evenly rounded overflow-hidden shadow-lg"
+              className="cursor-pointer hover:scale-105 duration-300 max-w-sm h-[23rem] bg-white flex flex-col rounded overflow-hidden shadow-lg"
               key={app.id}
               onClick={() => router.push(app.link)}
             >
-              <div className="px-6 py-4">
-                <p className="font-bold text-gray-700 text-xl mb-2">
-                  {app.title}
-                </p>
-                <p className="text-gray-700 text-sm">{app.description}</p>
+              <div className="border h-36 relative w-full">
+                <Image fill src={app.img}  alt="project" />
               </div>
-              <div className="flex items-center justify-center">
-                {app.libraries.map(l => (
-                  <button key={l.length * Math.random()} className="p-button">#{l}</button>
-                ))}
+              <div className="flex flex-col justify-evenly h-[13rem]">
+                <div className="px-6 py-4">
+                  <p className="font-bold text-gray-700 text-xl mb-2">
+                    {app.title}
+                  </p>
+                  <p className="text-gray-700 text-sm">{app.description}</p>
+                </div>
+                <div className="flex items-center justify-center">
+                  {app.libraries.map((l) => (
+                    <button key={l.length * Math.random()} className="p-button">
+                      #{l}
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["gorzelinski.com", "dwglogo.com"]
+  }
 }
 
 module.exports = nextConfig
